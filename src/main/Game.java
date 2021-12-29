e7bf9430def49705cd17b9ce2277b45a6499ac5d
Dalton Dayton <daltondayton1@gmail.com>
2021-12-29 15:30:26 -0700

    Add game loops. FPS and UPS.

--- src/main/Game.java
+++ src/main/Game.java
@@ -7,24 +7,25 @@ import java.io.InputStream;
 import javax.swing.JFrame;
 import javax.imageio.ImageIO;
 
-public class Game extends JFrame {
+public class Game extends JFrame implements Runnable {
 
     private GameScreen gameScreen;
-
     private BufferedImage img;
+    private Thread gameThread;
 
-    // Constructor
+    private final double FPS_SET = 120.0;
+    private final double UPS_SET = 60.0;
+
     public Game() {
-
         importImg();
 
-        setSize(700, 700);
-        setVisible(true);
+        setSize(640, 640);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // null opens in the center
 
         gameScreen = new GameScreen(img);
         add(gameScreen); // Add a JPanel to the JFrame
+        setVisible(true);
     }
 
     private void importImg() {
@@ -37,7 +38,50 @@ public class Game extends JFrame {
         }
     }
 
+    private void start() {
+        gameThread = new Thread(this) {
+        };
+
+        gameThread.start();
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
+        game.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+
+        long lastFrame = System.nanoTime();
+        long lastUpdate = System.nanoTime();
+        long lastTimeCheck = System.currentTimeMillis();
+
+        int frames = 0;
+        int updates = 0;
+
+        while (true) {
+            // Render
+            if (System.nanoTime() - lastFrame >= timePerFrame) {
+                repaint();
+                lastFrame = System.nanoTime();
+                frames++;
+            }
+
+            // Update
+            if (System.nanoTime() - lastUpdate >= timePerUpdate) {
+                lastUpdate = System.nanoTime();
+                updates++;
+            }
+
+            if (System.currentTimeMillis() - lastTimeCheck >= 1000) {
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+                lastTimeCheck = System.currentTimeMillis();
+            }
+        }
     }
 }
